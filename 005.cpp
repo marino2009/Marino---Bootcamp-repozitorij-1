d09a1e9f873dfc6b462402e016919c77784571ee
marino2009 <45363000+marino2009@users.noreply.github.com>
2018-11-27 09:35:53 +0100

    Prvi commit
    Added first file.
    Izmjena prva.

---
+++ 005.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+
+{
+
+// bla bla bla
+
+cout<<"x vrijednost\ty vrijednost\tizraz\trezultat"<<endl;
+cout<<"10\t\t5\t\tx=y+3\tx=8"<<endl;
+cout<<"10\t\t5\t\tx=y-2\tx=3"<<endl;
+cout<<"10\t\t5\t\tx=y*5\tx=25"<<endl;
+cout<<"10\t\t5\t\tx=x/y\tx=2"<<endl;
+cout<<"10\t\t5\t\tx=x%y\tx=0"<<endl;
+
+cout<<endl;
+cout<<"stupac1\tstupac2\tstupac3"<<endl;
+cout<<"x\to\tx"<<endl;
+cout<<"o\tx\to"<<endl;
+cout<<"x\to\tx"<<endl;
+cout<<"Ja sam Marino"<<endl;
+
+cout<<endl;
+cout<<"ja sam \'Marino'" <<endl;
+cout<<"ja sam \"Marino\"";
+
+return 0;
+}

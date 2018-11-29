98ce66fd29100943d485b5adeb9d136b04428eb0
marino2009 <45363000+marino2009@users.noreply.github.com>
2018-11-29 01:01:54 +0100

    Create 002.cpp

---
+++ 002.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	int a,b,c,result;
+	// varijable
+	a = 5;
+	b = 2;
+	c = a + 1;
+	result = a - b;
+	
+	cout<<result;
+	
+	return 0;
+
+}

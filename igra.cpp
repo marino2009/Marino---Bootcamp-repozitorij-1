6683b9ce6884147b8f9d48032804e4805d1649ff
marino2009 <45363000+marino2009@users.noreply.github.com>
2018-11-29 01:02:23 +0100

    Create igra.cpp

---
+++ igra.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	
+	//modul rješavanje!!!
+	int a,b,c,d,e;
+		cout << "unesi 4-znamenkasti: " << endl;
+	cin >> a;
+	b = (a / 1000);
+	c = ((a % 1000) / 100);
+	d = (((a % 1000) % 100) / 10);
+	e = (((a % 1000) % 100) % 10);
+	cout << b << endl << c << endl << d << endl << e << endl;
+
+	if (9>b, b>c, b>d, b>e) {
+		cout << (b+1) << endl;
+	}
+	else if (9 > c, c >= b, c >= d, c>=e) {
+		cout << (c+1) << endl;
+	}
+	else if (9 > d,d>=b,d>=c,d>=e) {
+		cout << (d+1) << endl;
+	}
+	else if (9 > e,e>=b,e>=c,e>=d) {
+		cout << (e+1) << endl;
+	}
+	else {
+		cout << "9" << endl;
+	}
+}
